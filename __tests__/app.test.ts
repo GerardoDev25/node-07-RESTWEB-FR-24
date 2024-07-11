185ed92a568771e97e899ff97557135765d3af7b
Gerardo Miranda <gerardodev25@gmail.com>
2024-07-10 20:28:00 -0400

    test(test): :white_check_mark: testing in app.ts

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,5 +1,22 @@
+import { envs } from '../src/config/config';
+import { Server } from '../src/presentation/server';
+
+jest.mock('../src/presentation/server');
+
 describe('app.ts', () => {
-  test('should work', () => {
-    expect(true).toBeTruthy();
+  test('should call server with arguments and start', async () => {
+    await import('../src/app');
+
+    expect(Server).toHaveBeenCalledTimes(1);
+    expect(Server).toHaveBeenCalledWith({
+      port: envs.PORT,
+      publicPath: envs.PUBLIC_PATH,
+      router: expect.any(Function),
+    });
+
+    expect(Server.prototype.start).toHaveBeenCalledTimes(1);
+    expect(Server.prototype.start).toHaveBeenCalledWith();
+    
   });
+  
 });
